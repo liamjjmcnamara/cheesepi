e02c3bf8e759444c625dcb320d9a59d76d3b13ee
Liam McNamara <liamjjmcnamara@gmail.com>
2015-03-20 18:12:23 +0100

    test the distribution before pushing

--- makeDistribution.sh
+++ makeDistribution.sh
@@ -1,4 +1,22 @@
+# cd to the directory this file is in
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR
 
-# take the current version of the repos and place it on the distribution server
-# for the Pis to download from.
-rsync -avzhe ssh dist/* pi@grayling.sics.se:cheesepi
+# ensure we have the most recent commit from the repos
+git pull
+
+# place a commit SHA-1 in the distribution dir
+git log --pretty=format:'%h' -n 1 > dist/version
+echo >> dist/version
+
+
+# Run tests, make sure the code works
+./testDistribution.py
+# if it failed, whine and exit
+if [ $? -ne 0 ]; then
+	echo "Tests failed! Not pushing code"
+else
+	# take the current version of the repos and place it on the distribution server
+	# for the Pis to download from.
+	rsync -avzhe ssh dist/* pi@grayling.sics.se:cheesepi
+fi
