065810b743cd0fc2982ea55b8b35ab486535da81
Liam McNamara <liamjjmcnamara@gmail.com>
2015-03-20 16:25:26 +0100

    rsync nicer names

--- dist/update.sh
+++ dist/update.sh
@@ -1,3 +1,3 @@
 
 # pull the latest files from the server
-rsync -avzhe ssh  pi@grayling.sics.se:dist /usr/local/cheesepi
+rsync -avzhe ssh  pi@grayling.sics.se:cheesepi /usr/local/cheesepi
--- makeDistribution.sh
+++ makeDistribution.sh
@@ -1,4 +1,4 @@
 
 # take the current version of the repos and place it on the distribution server
 # for the Pis to download from.
-rsync -avzhe ssh dist pi@grayling.sics.se:dist
+rsync -avzhe ssh dist/* pi@grayling.sics.se:cheesepi
