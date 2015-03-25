9d7a97090263a977bf72bbb6922fb62d48cf5111
Liam McNamara <liamjjmcnamara@gmail.com>
2015-03-25 19:45:53 +0100

    -extended install.sh script -added git clone for public users to update.py

--- dist/install/install.sh
+++ dist/install/install.sh
@@ -1,10 +1,21 @@
 
-# Install required software
+# Where shall we install CheesePi?
+INSTALL_DIR = /usr/local/cheesepi
+
+# Install required OS software
 apt-get install mysql
 
+# install python software modules
+pip install MySQL-python
 
 # Make main CheesePi directory
-mkdir -p /usr/local/cheesepi
+mkdir -p $INSTALL_DIR
 
-# Copy cheesepi.conf
-# cp cheesepi.default.conf cheesepi.conf
+# Copy cheesepi.conf if it doesnt exist
+if [ ! -f $INSTALL_DIR/cheesepi.conf ]; then
+	cp $INSTALL_DIR/cheesepi.default.conf $INSTALL_DIR/cheesepi.conf
+fi
+
+# Make mysql database and user
+$INSTALL_DIR/install/install.sh Measure pi password
+
--- dist/update.sh
+++ dist/update.sh
@@ -2,3 +2,6 @@
 
 # pull the latest files from the server
 sudo rsync -avzhe ssh  pi@grayling.sics.se:cheesepi/dist/* /usr/local/cheesepi/
+
+# Perform git clone from a read-only user account with no git repos history
+#git clone --depth=1 https://bitbucket.org/IanRobinMarsh/cheesepi.git
