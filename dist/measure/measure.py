a5b55b1c469254a9a1486b518942d4edccd47ca6
Liam McNamara <liamjjmcnamara@gmail.com>
2015-03-26 21:30:09 +0100

    cleaning..

--- dist/measure/measure.py
+++ dist/measure/measure.py
@@ -55,7 +55,7 @@ def measure(config=None):
     # Run the measurement suite
     updatecall = [config['cheesepi_dir']+"/update.sh"]
     run(updatecall)
-    
+
     targets = config['landmarks'].split()
     pingcall = [config['cheesepi_dir']+"/measure/pingMeasure.py"]
     for target in targets:
