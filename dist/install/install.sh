7b031f2c9d98b89ed89c1a71a8b2419cef67e267
Liam McNamara <liamjjmcnamara@gmail.com>
2015-03-26 22:39:07 +0100

    make executable

--- dist/install/install.sh
+++ dist/install/install.sh
@@ -2,7 +2,7 @@
 INSTALL_DIR=/usr/local/cheesepi/
 
 # Optionally update apt-get
-sudo apt-get update && sudo apt-get upgrade
+#sudo apt-get update && sudo apt-get upgrade
 
 # make a local distribution
 sudo rsync -avzhe ssh  pi@grayling.sics.se:dist/* $INSTALL_DIR
@@ -14,7 +14,7 @@ sudo apt-get install mysql-client mysql-server python-mysqldb
 
 # Copy cheesepi.conf if it doesnt exist
 if [ ! -f $INSTALL_DIR/cheesepi.conf ]; then
-	cp $INSTALL_DIR/cheesepi.default.conf $INSTALL_DIR/cheesepi.conf
+	sudo cp $INSTALL_DIR/cheesepi.default.conf $INSTALL_DIR/cheesepi.conf
 fi
 
 # Make mysql database and user
