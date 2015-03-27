8d3f445c679e0c3f9829115c37a991d8ce06cc4a
biniam.gebregergs <biniam.gebregergs@gmail.com>
2015-03-27 16:40:15 +0100

    The database analyser checks the database if every pi sends data

--- server/analyseDatabase.py
+++ server/analyseDatabase.py
@@ -6,45 +6,50 @@ import MySQLdb as mdb
 
 
 def main():
-        listOfPIs = pi_List()
-        pis_Not_Send_Data(listOfPIs)
-
+	connectionDB = mdb.connect("localhost", "urban", "basketphone", "sicspi")
+	cur = connectionDB.cursor()
+        listOfPIs = pi_List(connectionDB, cur)
+        pis_Not_Send_Data(listOfPIs,connectionDB, cur)
+	connectionDB.close()
 #A method that list out pis who do not send data today
-def pis_Not_Send_Data(listOfPIs):
+def pis_Not_Send_Data(listOfPIs,connectionDB, cur):
         now = datetime.datetime.now()
-        today = now.strftime("%m-%d-%Y")
-        now = now.strftime("%Y-%m-%d %H:%M")
-        #the path the received tables placed in the central server      
-        path = "/home/pi/Buffer/"
+        now = now.strftime("%Y-%m-%d")
+        now = datetime.datetime.strptime(now, "%Y-%m-%d")
         #The list Pis which do not send data will be save in a file
         myfile = open("ListPIs.txt",'a+')
-        myfile.write("\nPis which do not send data today: " + now)
+        myfile.write("\nPis which do not send data today: " + str(now))
         pisNotSendData = []
+	print "Start looking for Pis who do not send data"
+        #Iterating on the registered Pis
         for pi in listOfPIs:
-                #the file name is the combination of Pi's MAC and current date
-                fname = str(pi[0]) + today
-                #Checks if the file starts with combination of MAC and current date exist in the directory
-                for i in os.listdir(path):
-                         if os.path.isfile(os.path.join(path,i)) and fname in i:
-                                break
-                #if it doesn't add to the list of pis who do not send data
-                else:
-                       pisNotSendData.append(str(pi[0]))
-        #if the list is not empty add the content to the file
+                #Retrieves the starting time of the last data send by the PI  
+		selectQuery = """SELECT StartTime FROM Operation,Ping,PI 
+				 WHERE Operation.OID = Ping.OID AND Ping.PID = PI.PID AND 
+                                 PI.EthMAC = '%s' ORDER BY Operation.StartTime DESC LIMIT 1;"""%str(pi[0])
+                cur.execute(selectQuery)
+		startTime = cur.fetchall()
+		connectionDB.commit()
+		startTime = startTime[0][0].strftime("%Y-%m-%d")
+		startTime = datetime.datetime.strptime(startTime, "%Y-%m-%d")
+		difference = now - startTime
+		print difference
+		#If the starting time is not today
+		if (difference.days > 0):
+			pisNotSendData.append(str(pi[0])) 
         if pisNotSendData:
 		for pi in pisNotSendData:
                         myfile.write("\n" + str(pi))
         myfile.close()
+	print "Writes the list of Pis who do not send data in the file"
 
 #A metho that returns list of registerd pis in the central server
-def pi_List():
-        connectionDB = mdb.connect("localhost", "urban", "basketphone", "sicspi")
-        cur = connectionDB.cursor()
+def pi_List(connectionDB, cur):
         selectQuery = """SELECT EthMAC from PI"""
         cur.execute(selectQuery)
         rows = cur.fetchall()
         connectionDB.commit()
-        connectionDB.close()
+        #connectionDB.close()
         return rows
 
 
