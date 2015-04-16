acda9b7edd29777781a26918a8f936da4b4611f1
Liam McNamara <liamjjmcnamara@gmail.com>
2015-04-16 22:42:55 +0200

    trying out some install script behaviour

--- cheesepi/install/install.sh
+++ cheesepi/install/install.sh
@@ -1,24 +1,39 @@
 # Where shall we install CheesePi?
-INSTALL_DIR=/usr/local/cheesepi/
+INSTALL_DIR=/usr/local/cheesepi
 
-echo "Please enter root pass to enable apt-get software install:"
+echo "Enter root pass to enable apt-get software install if prompted..."
 # Install required OS software
-# include ntpdate ?
-sudo apt-get update
+#sudo apt-get update
 sudo apt-get install httping python-pip
+# include ntpdate ?
 
 # add python modules
-sudo pip install cherrypy influxdb
+pip install cherrypy influxdb
 
-# Copy influx config if it doesnt exist
-#if [ ! -f $INSTALL_DIR/cheesepi.conf ]; then
-#	sudo cp $INSTALL_DIR/cheesepi.default.conf $INSTALL_DIR/cheesepi.conf
-#fi
+
+# Copy Influx config if it doesnt exist
+if [ ! -f $INSTALL_DIR/tools/influxdb/config.toml ]; then
+	sudo cp $INSTALL_DIR/tools/influxdb/config.sample.toml $INSTALL_DIR/tools/influxdb/config.toml
+fi
+
+# Start Influx database server
+INFLUX_DIR=$INSTALL_DIR/tools/influxdb
+INFLUX_CMD="$INFLUX_DIR/influxdb -config=$INFLUX_DIR/config.toml"
+$INFLUX_CMD
+
+# ..and have it start on boot
+#sudo echo $INFLUX_CMD >> /etc/rc.local
+#echo $INFLUX_CMD | sudo tee --append /etc/rc.local
+#echo "C1:2345:boot:$INFLUX_CMD" >> /etc/inittab
+
+# Create 'cheesepi' database
+#$INSTALL_DIR/install/makeDB.py
 
 
 # Intall a crontab entry so that $INSTALL_DIR/measure/measure.py is run
 # should avoid duplication...
-echo -e "#cheesepi measurement\n*/5 * * * * python $INSTALL_DIR/measure/measure.py &" >> /etc/crontab
+#sudo echo "*/5 * * * * python $INSTALL_DIR/measure/measure.py" >> /etc/crontab
+echo "*/5 * * * * python $INSTALL_DIR/measure/measure.py" | sudo tee --append /etc/crontab
 
-# Try to run it
+# Try to run the measure script
 $INSTALL_DIR/measure/measure.py 
--- cheesepi/storage/__init__.py
+++ cheesepi/storage/__init__.py
@@ -3,15 +3,15 @@ import dao
 try:
 	import dao_mongo
 except ImportError:
-	print "Missing InfluxDB python module (and GridFS and bson), use 'pip install influxdb'"
+	print "Missing Mongo python module (and GridFS and bson), use 'pip install pymongo'"
 
 try:
 	import dao_influx
 except ImportError:
-	print "Missing InfluxDB python module (and GridFS and bson), use 'pip install influxdb'"
+	print "Missing InfluxDB python module, use 'pip install influxdb'"
 
 try:
 	import dao_mysql
 except ImportError:
-	print "Missing InfluxDB python module (and GridFS and bson), use 'pip install influxdb'"
+	print "Missing MySQL python module, use 'pip install MySQL-python'"
 
