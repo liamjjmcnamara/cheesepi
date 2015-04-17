83f952c05a60672dd8ed1369785b3c6cc2980fd0
Liam McNamara <liamjjmcnamara@gmail.com>
2015-04-17 12:05:17 +0200

    just need local grafana config file generation now...

--- cheesepi/install/install.sh
+++ cheesepi/install/install.sh
@@ -20,29 +20,31 @@ fi
 INFLUX_DIR=$INSTALL_DIR/tools/influxdb
 INFLUX_CMD="$INFLUX_DIR/influxdb -config=$INFLUX_DIR/config.toml"
 $INFLUX_CMD &
+# and the webserver serving a grafana dashboard
+$INSTALL_DIR/webserver/webserver.py &
 
-# ..and have it start on boot
+# ..and have both start on boot
 #sudo echo $INFLUX_CMD >> /etc/rc.local
 #echo $INFLUX_CMD | sudo tee --append /etc/rc.local
 if ! grep --quiet influxdb /etc/inittab; then
-	echo "C1:2345:boot:$INFLUX_CMD" | sudo tee --append /etc/inittab
+	echo -e "\nC1:2345:boot:$INFLUX_CMD" | sudo tee --append /etc/inittab
+	echo "W1:2345:boot:$INSTALL_DIR/webserver/webserver.py" | sudo tee --append /etc/inittab
 fi
 
-# Create Influx 'cheesepi' database
+# Create Influx 'cheesepi' databases
 #$INSTALL_DIR/install/makeInfluxDB.py
-# Very quick and dirty solution:
+# Very quick and dirty solution
 curl -s "http://localhost:8086/db?u=root&p=root" -d "{\"name\": \"cheesepi\"}"
 curl -s "http://localhost:8086/db?u=root&p=root" -d "{\"name\": \"grafana\"}"
 
 
 # Intall a crontab entry so that $INSTALL_DIR/measure/measure.py is run
-# should avoid duplication...
-#sudo echo "*/5 * * * * python $INSTALL_DIR/measure/measure.py" >> /etc/crontab
 if ! grep --quiet measure.py /etc/crontab; then
-	echo "*/5 * * * * python $INSTALL_DIR/measure/measure.py" | sudo tee --append /etc/crontab
+	echo -e "\n*/5 * * * * python $INSTALL_DIR/measure/measure.py" | sudo tee --append /etc/crontab
 fi
 
 # Try to run the measure script
 #$INSTALL_DIR/measure/measure.py 
 
+# Ensure the "cheesepi" module can be imported
 echo -e "\nAdd '/usr/local' to your python search path, append the following to ~/.profile\nexport PYTHONPATH=/usr/local\n"
