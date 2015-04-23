edfd9bb550f4fa73fceeb0697c86a7bd7ff77033
Liam McNamara <liamjjmcnamara@gmail.com>
2015-04-23 09:42:36 +0200

    better ordering for install steps

--- cheesepi/install/install.sh
+++ cheesepi/install/install.sh
@@ -5,26 +5,6 @@ INSTALL_DIR=/usr/local/cheesepi
 # To be used in the grafana configuration (so it knows where the Influx DB is)
 LOCAL_IP=`hostname -I |head -n1| tr -d '[[:space:]]'`
 
-# Start Influx database server and webserver in the background
-# Do this first, as Influx needs to spool up
-INFLUX_DIR=$INSTALL_DIR/tools/influxdb
-INFLUX_CMD="$INFLUX_DIR/influxdb -config=$INFLUX_DIR/config.toml"
-nohup $INFLUX_CMD &
-# and the webserver serving a grafana dashboard
-sleep 10
-$INSTALL_DIR/webserver/webserver.py &
-
-echo "Enter root pass to enable apt-get software install if prompted..."
-# Install required OS software
-#sudo apt-get update
-# Speed improvements (through a binary module)
-#sudo apt-get install build-essential python-dev
-sudo apt-get install httping python-pip python-mysqldb
-# include ntpdate ?
-
-# add python modules
-sudo pip install cherrypy influxdb pymongo
-
 
 # Copy Influx config if it doesnt exist
 if [ ! -f $INSTALL_DIR/tools/influxdb/config.toml ]; then
@@ -34,7 +14,27 @@ fi
 if [ ! -f $INSTALL_DIR/webserver/dashboard/config.js ]; then
 	cat $INSTALL_DIR/webserver/dashboard/config.sample.js| sed "s/my_influxdb_server/$LOCAL_IP/" >$INSTALL_DIR/webserver/dashboard/config.js
 fi
+# Start Influx database server and webserver in the background
+# Do this first, as Influx needs to spool up
+INFLUX_DIR=$INSTALL_DIR/tools/influxdb
+INFLUX_CMD="$INFLUX_DIR/influxdb -config=$INFLUX_DIR/config.toml"
+nohup $INFLUX_CMD &
+# and the webserver serving a grafana dashboard
+$INSTALL_DIR/webserver/webserver.py &
+sleep 5
 
+
+# Install required OS software
+echo "Enter root pass to enable apt-get software install if prompted..."
+#sudo apt-get update
+# Speed improvements (through a binary module)
+#sudo apt-get install build-essential python-dev
+sudo apt-get install httping python-pip python-mysqldb
+# include ntpdate ?
+# add python modules
+sudo pip install cherrypy influxdb pymongo
+
+sleep 5
 
 # ..and have both start on boot
 #sudo echo $INFLUX_CMD >> /etc/rc.local
@@ -52,12 +52,12 @@ fi
 
 # Create Influx 'cheesepi' and 'grafana' databases
 # Very quick and dirty solution
-echo "Waiting 20s for Influx to startup..."
+echo "Waiting for Influx to startup..."
 sleep 15
 curl -s "http://localhost:8086/db?u=root&p=root" -d "{\"name\": \"cheesepi\"}"
 curl -s "http://localhost:8086/db?u=root&p=root" -d "{\"name\": \"grafana\"}"
 
 
-# Ensure the "cheesepi" module can be imported
-echo -e "\nAdd '/usr/local' to your python search path, append the following to ~/.profile\nexport PYTHONPATH=/usr/local\n"
+
+echo -e "\nVisit $LOCAL_IP:8080/dashboard to see your dashboard!\n"
 
