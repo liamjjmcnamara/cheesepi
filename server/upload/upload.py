0fc9e93faf836e63e72ab331c15476bd798ed932
Liam McNamara <liamjjmcnamara@gmail.com>
2015-04-27 11:02:32 +0200

    records who uploaded and who they claim to be

--- server/upload/upload.py
+++ server/upload/upload.py
@@ -1,36 +1,47 @@
 import os
-import random
 import sys
-import cgi
-import cgitb
-cgitb.enable()
-# to avoid showing errors to users use the following
-#cgitb.enable(display=0, logdir="/path/to/logdir")
+import datetime
+from mod_python import apache
 
-#form = cgi.FieldStorage()
-dump_dir="/var/www/html/dump"
-if not os.path.exists(dump_dir):
-    print "making dir"
-    os.makedirs(dump_dir)
-
-
+NOW = str(datetime.datetime.utcnow().strftime("%s"))
+DUMP_DIR="/var/www/html/dump"
+if not os.path.exists(DUMP_DIR):
+	os.makedirs(DUMP_DIR)
 
 def index(req):
-   if not 'file' in req.form:
-       return "Error: Please upload a file using"
+	if not 'file' in req.form or not req.form['file'].filename:
+		return "Error: Please upload a file"
 
-   # A nested FieldStorage instance holds the file
-   fileitem = req.form['file']
+	# Record which IP send this file
+	ip = req.get_remote_host(apache.REMOTE_NOLOOKUP)
+	
+	user = auth_user(req.form['ethmac'],"pass?")
+	return save_file(req.form['file'], ip, user)
 
-   # Test if the file was uploaded
-   if fileitem.filename:
-      # strip leading path from file name to avoid directory traversal attacks
-      filename = os.path.basename(fileitem.filename)
-      # build absolute path to files directory
-      #dump_dir = os.path.join(os.path.dirname(filename), 'dump')
-      open(os.path.join(dump_dir, filename), 'wb').write(fileitem.file.read())
-      message = 'The file "%s" was uploaded successfully' % filename
+def ensure_dir(path):
+	if not os.path.exists(path):
+		os.makedirs(path)
 
-   else:
-      message = 'Error: No file was uploaded'
-   return message
+def auth_user(mac,path):
+	return mac
+
+def save_file(fileitem, ip, user=None):
+	# strip leading path from file name to avoid directory traversal attacks
+	filename = NOW+".tar.gz"
+	if user!=None:
+		filename = user+"-"+filename
+	#os.path.basename(fileitem.filename)
+
+	# build absolute path to files directory
+	host_dir = os.path.join(DUMP_DIR, ip) 
+	ensure_dir(host_dir)
+	filepath = os.path.join(host_dir, filename)
+
+	fd = open(filepath, 'wb')
+	while 1:
+        	chunk = fileitem.file.read(100000)
+        	if not chunk: break
+        	fd.write (chunk)
+	message = 'The file "%s" was uploaded successfully from  %s' % (filepath, ip)
+
+	return message
