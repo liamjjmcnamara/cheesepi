d1ea927450332cb5c5433c92fca57d0a9d753c5f
Liam McNamara <liamjjmcnamara@gmail.com>
2015-05-06 19:51:32 +0000

    save dead code in repos

--- cheesepi/storage/dump_db.py
+++ cheesepi/storage/dump_db.py
@@ -5,6 +5,7 @@ import zlib
 import io
 import zipfile
 import StringIO
+import tempfile
 import requests
 
 sys.path.append("/usr/local/")
@@ -15,24 +16,33 @@ dump_url = "http://cheesepi.sics.se/upload.py"
 
 def perform_database_dump():
     dao = cheesepi.config.get_dao()
-    ethmac = cheesepi.utils.getCurrMAC()
 
     last_updated = cheesepi.config.get_last_updated(dao)
     dump_data = dao.dump(last_updated)
-
+    ethmac = cheesepi.utils.getCurrMAC()
     parameters = {'ethmac': ethmac}
     #files = {'file': ('unusedfilename.tar.gz', zlib.compress(dump_data)), }
-
     #fd = io.BytesIO(dump_data)
     #zdata = zipfile.ZipFile(fd)
+    fd = tempfile.TemporaryFile()
+    zfd = zipfile.ZipFile(fd,'w', zipfile.ZIP_DEFLATED)
+    zfd.writestr("file.z",dump_data)
+    fd.flush()
+    fd.seek(0)
+
+    #buff = StringIO.StringIO()
+    #zip_archive = zipfile.ZipFile(buff, mode='w')
+    #zip_archive.writestr("file.dat",dump_data)
+    #zip_archive.close()
     
-    zfd = zipfile.ZipFile(StringIO.StringIO(), 'a')
-    zfd.writestr("filename.z",dump_data)
-    zfd.close()
-    files = {'file': ('unusedfilename.tar.gz', zfd), }
+    #zfd = zipfile.ZipFile(StringIO.StringIO(), 'a')
+    #zfd.writestr("filename.z",dump_data)
+    #zfd.close()
+    files = {'file': ('archive.z', fd), }
 
     r = requests.post(dump_url, data=parameters, files=files)
     print r.text
+    #fd.close()
 
 
 if __name__ == "__main__":
