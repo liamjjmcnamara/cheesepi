a96649ab56e80e488ee647c0f89e5d214856b7b5
Liam McNamara <liamjjmcnamara@gmail.com>
2015-05-06 07:23:17 +0000

    regex parsing of traceroute response

--- cheesepi/measure/tracerouteMeasure.py
+++ cheesepi/measure/tracerouteMeasure.py
@@ -54,15 +54,15 @@ import cheesepi
 
 
 def measure(dao, landmarks, saveToFile=False):
-	trc = Traceroute()
 	hoplist = []
 	#Extract the ethernet MAC address of the PI
 	for landmark in landmarks:
 		startTime = cheesepi.utils.now()
-		tracerouteResult = getData(landmark)
+		output = getData(landmark)
 		endTime = cheesepi.utils.now()
-		trc, hoplist = reformat(tracerouteResult, startTime, endTime)
-		insertData(dao, trc, hoplist)
+		#trc, hoplist = reformat(tracerouteResult, startTime, endTime)
+		traceroute, hoplist = parse(output, startTime, endTime)
+		insertData(dao, traceroute, hoplist)
 
 #Execute traceroute function
 def getData(target):
@@ -74,22 +74,53 @@ def getData(target):
 	result.stdout.flush()
 	return ret
 
-#A Traceroute class to represent the Traceroute table
-class Traceroute(object):
-	StartingTime = None
-	EndingTime = None
-	SourceAddress = None
-	DestinationDomain = None
-	DestinationAddress = None
 
-#A Hop class to represent the Hop table
-class Hop(object):
-	_id = None
-	HopNumber = None
-	PacketNumber = None
-	PacketDestinationAddress = None
-	PacketDestinationDomain = None
-	RTT = None
+def parse_null(hop_count):
+	return {'hop_count': hop_count,
+		'domain1': "*", 'domain2': "*", 'domain3': "*",
+		'ip1'    : "*", 'ip2'    : "*", 'ip3'    : "*",
+		'delay1': -1, 'delay2': -1, 'delay3': -1, }
+
+#############################
+# Parse Linux command
+#
+
+def parse(data, start_time, end_time):
+	lines = data.split("\n")
+	traceroute = parse_destination(lines[0], start_time, end_time)
+	hops=[]
+	for line in lines[1:-1]:
+		hop_count = int(line[:3].strip())
+		hops.append(parse_hop(hop_count, line[4:]))
+	print "hops: ",hops
+	return traceroute, hops
+
+def parse_destination(destination, start_time, end_time):
+	traceroute = {}
+	fields = destination.split()
+	traceroute['domain']     = fields[2]
+	traceroute['ip']         = fields[3][1:-1]
+	traceroute['start_time'] = start_time
+	traceroute['end_time']   = end_time
+	return traceroute
+
+def parse_hop(hop_count, host_line):
+	"""This does not yet deal with network problems"""
+	hop={'hop_count':hop_count}
+	retry="1" # string accumulator
+	for match in re.finditer(r"\*|(([\w\.-]+) \(([\d\.]+)\)  ([\d\.ms ]+) )", host_line):
+		if match.group(0)=="*": # found a non response
+			hop['domain'+retry]="*"
+			hop['ip'+retry]="*"
+			hop['delay'+retry]="-1"
+		else: # some host reploiued N times
+			for delay in match.group(4).split("ms"):
+				hop['domain'+retry]= match.group(2)
+				hop['ip'+retry]    = match.group(3)
+				hop['delay'+retry] = delay
+				retry = str(int(retry)+1) # inc but keep as string
+		retry = str(int(retry)+1) # inc but keep as string
+	return hop
 
 
 #########################
@@ -119,23 +150,12 @@ def parse_mac(data):
 	print hops
 	return hops
 
-def parse_null(hop_count):
-	return {'hop_count': hop_count,
-		'domain1': "*", 'domain2': "*", 'domain3': "*",
-		'ip1'    : "*", 'ip2'    : "*", 'ip3'    : "*",
-		'delay1': -1, 'delay2': -1, 'delay3': -1, }
 
 def parse_hop_1host(hop_count, host_fields):
 	return {'hop_count': hop_count,
-		'domain1': host_fields[0],
-		'domain2': host_fields[0],
-		'domain3': host_fields[0],
-		'ip1'    : host_fields[1],
-		'ip2'    : host_fields[1],
-		'ip3'    : host_fields[1],
-		'delay1': host_fields[2],
-		'delay2': host_fields[4],
-		'delay3': host_fields[6],
+		'domain1': host_fields[0], 'domain2': host_fields[0], 'domain3': host_fields[0],
+		'ip1'    : host_fields[1], 'ip2'    : host_fields[1], 'ip3'    : host_fields[1],
+		'delay1': host_fields[2], 'delay2': host_fields[4], 'delay3': host_fields[6],
 		}
 
 def parse_hop_3host(hop_count, retry1, retry2, retry3):
@@ -143,127 +163,24 @@ def parse_hop_3host(hop_count, retry1, retry2, retry3):
 	retry2_fields = retry2.split()
 	retry3_fields = retry3.split()
 	return {'hop_count': hop_count,
-		'domain1': retry1_fields[0],
-		'domain2': retry1_fields[0],
-		'domain3': retry1_fields[0],
-		'ip1'   : retry1_fields[1],
-		'ip2'   : retry2_fields[1],
-		'ip3'   : retry3_fields[1],
-		'delay1': retry1_fields[2],
-		'delay2': retry2_fields[2],
-		'delay3': retry3_fields[2],
+		'domain1': retry1_fields[0], 'domain2': retry1_fields[0], 'domain3': retry1_fields[0],
+		'ip1'   : retry1_fields[1], 'ip2'   : retry2_fields[1], 'ip3'   : retry3_fields[1],
+		'delay1': retry1_fields[2], 'delay2': retry2_fields[2], 'delay3': retry3_fields[2],
 		}
-
-#############################
-
-def parse(data):
-	hops=[]
-	lines = data.split()
-	for line in lines[1:]:
-		hop_count   = int(line[:3].strip())
-		host_line   = line[4:] # extract everything after hopcount
-		host_fields = host_line.split()
-		if len(host_fields)==3:
-			hops.extend(parse_null(hop_count))
-		else:
-			hop_entries = parse_hop(hop_count, host_fields)
-			hops.extend(hop_entries)
-	print hops
-	return hops
-
-def parse_hop(hop_count, host_fields):
-	if   len(host_fields)==8:
-		return {'hop_count': hop_count,
-			'domain1':host_fields[0],       'domain2':host_fields[0],        'domain3':host_fields[0],
-			'ip1'    :host_fields[1][1:-1], 'ip2'    :host_fields[1][1:-1],  'ip3'    :host_fields[1][1:-1],
-			'delay1' :float(host_fields[2]),'delay2' :float(host_fields[4]), 'delay3' :float(host_fields[6]),
-			}
-	elif len(host_fields)==11:
-		return {'hop_count': hop_count,
-			'domain1':host_fields[0],       'domain2':host_fields[4],       'domain3':host_fields[8],
-			'ip1'    :host_fields[1][1:-1], 'ip2'    :host_fields[5][1:-1], 'ip3'    :host_fields[9][1:-1],
-			'delay1' :float(host_fields[2]),'delay2' :float(host_fields[6]),'delay3' :float(host_fields[10]),
-			}
-	else:
-		print "Error parsing host line: "+str(host_fields)
-		exit(1)
-
-
-#read the data from traceroute and reformat for database entry
-def reformat(data, startTime, endTime):
-	trc = Traceroute()
-	#hop = Hop()
-	hoplist = []
-	print "Structuring the traceroute result"
-	trc.StartingTime = startTime
-	trc.EndingTime = endTime
-	#print data
-	lines = data.split("\n")
-	tmp = lines[0].split()
-	trc.DestinationDomain = str(tmp[2])
-	trc.DestinationAddress = re.sub("[(),]", "", str(tmp[3]))
-	#print len(lines)
-	for line in lines[1:len(lines)-1]:
-		i = 1 # Line length counter
-		j = 1 #packet counter
-		hop = Hop()
-		ip = None
-		domain = None
-		temp = line.split()
-		#print temp[0]
-		hop.HopNumber = int(temp[0])
-		#till the end of the line
-		while i < len(temp):
-			#print temp[i]
-			if temp[i] == "*":
-				hop.PacketNumber = j
-				hop.PacketDestinationAddress = None
-				hop.PacketDestinationDomain = None
-				hop.RTT = -1
-				hopcopy = copy.copy(hop)
-				hoplist.append(hopcopy)
-				i = i + 1
-				j = j + 1
-			elif "(" in temp[i+1]:
-				hop.PacketNumber = j
-				hop.PacketDestinationAddress = re.sub("[(),]", "",str(temp[i+1]))
-				hop.PacketDestinationDomain =  str(temp[i])
-				hop.RTT = float(temp[i+2])
-				hopcopy = copy.copy(hop)
-				hoplist.append(hopcopy)
-				i = i + 4
-				j = j + 1
-
-			elif temp[i+1] == "ms":
-				hop.PacketNumber = j
-				hop.PacketDestinationAddress = ip
-				hop.PacketDestinationDomain = domain
-				hop.RTT = float(str(temp[i]))
-				hopcopy = copy.copy(hop)
-				hoplist.append(hopcopy)
-				i = i + 2
-				j = j + 1
-			ip = hop.PacketDestinationAddress
-			domain = hop.PacketDestinationDomain
-
-	return trc,hoplist
+###
 
 
 #insert the tracetoute results into the database
-def insertData(dao, trc, hoplist):
-	#insertIntoTraceroute = """INSERT INTO Traceroute(sourceAddress, destinationDomain, destinationAddress,
-	#	startingTime, endingTime, ethernetMacAddress, currentMacAddress)
-	#	values (%s,%s,%s,%s,%s,%s,%s)"""
-	#insertIntoHop = """INSERT INTO Hop(ID, hopNumber, packetNumber, packetDomainAddress, packetDestinationAddress,
-	#	RTT) values (%s,%s,%s,%s,%s,%s)"""
-
+def insertData(dao, traceroute, hoplist):
 	print "Writting to the Traceroute tabele"
-	traceroute_id = dao.write_op("traceroute", trc.as_dict())
+	traceroute_id = dao.write_op("traceroute", traceroute)
 
 	print "writing to the Hop table"
 	for hop in hoplist:
-		hop.traceroute = traceroute_id
-		dao.write_op("traceroot_hop",hop.as_dict())
+		print hop
+		#hop.traceroute = traceroute_id
+		hop['traceroute_id'] = traceroute_id
+		dao.write_op("traceroot_hop",hop)
 
 
 #parses arguments
