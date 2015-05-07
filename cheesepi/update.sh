e9755b1bdb78abd3a7b2aa736a8ed34b2bfcd5e7
Liam McNamara <liamjjmcnamara@gmail.com>
2015-05-07 15:33:01 +0200

    house keeping

--- cheesepi/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# pull the latest files from the server
-sudo rsync -avzhe ssh  pi@grayling.sics.se:dist/* /usr/local/cheesepi/
-
-# Perform git clone from a read-only user account with no git repos history
-#git clone --depth=1 https://bitbucket.org/IanRobinMarsh/cheesepi.git
