b36b9d455bb72b78235058bc23a24c9ac35857a3
Liam McNamara <liamjjmcnamara@gmail.com>
2015-05-07 14:24:57 +0000

    sudo everythign and give more newline

--- cheesepi/install/start_servers.sh
+++ cheesepi/install/start_servers.sh
@@ -5,11 +5,11 @@ INSTALL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 # Influx needs to spool up
 INFLUX_DIR=$INSTALL_DIR/tools/influxdb
 INFLUX_CMD="$INFLUX_DIR/influxdb -config=$INFLUX_DIR/config.toml"
-echo "Running: $INFLUX_CMD"
-nohup $INFLUX_CMD >&/dev/null &
+echo -e "\nRunning: $INFLUX_CMD"
+nohup sudo $INFLUX_CMD >&/dev/null &
 
 # and the webserver serving a grafana dashboard
 echo "Running: $INSTALL_DIR/webserver/webserver.py"
-$INSTALL_DIR/webserver/webserver.py &
+sudo $INSTALL_DIR/webserver/webserver.py &
 sleep 5
 
