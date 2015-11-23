ed48bffaf90785d0860be34a5847ff281d66bc17
Liam McNamara <liamjjmcnamara@gmail.com>
2015-11-23 10:09:23 +0100

    added httping and some json wrangling, for task building/transmission

--- cheesepi/tasks/Httping.py
+++ cheesepi/tasks/Httping.py
@@ -4,13 +4,12 @@ import os
 import re
 import logging
 import socket
-import json
 from subprocess import Popen, PIPE
 
 sys.path.append("/usr/local/")
 import cheesepi
 
-class Ping(cheesepi.tasks.Task):
+class Httping(cheesepi.tasks.Task):
 
     # construct the process and perform pre-work
     def __init__(self, dao, parameters):
@@ -20,13 +19,6 @@ class Ping(cheesepi.tasks.Task):
         self.ping_count  = 10 #parameters['ping_count']
         self.packet_size = 64 #parameters['packet_size']
         socket.gethostbyname(self.landmark) # we dont care, just populate the cache
-
-    def toDict(self):
-        return {'taskname'   :self.taskname,
-                'landmark'   :self.landmark,
-                'ping_count' :self.ping_count,
-                'packet_size':self.packet_size,
-                }
 
     # actually perform the measurements, no arguments required
     def run(self):
--- cheesepi/tasks/Task.py
+++ cheesepi/tasks/Task.py
@@ -1,3 +1,5 @@
+import json
+
 import cheesepi
 
 # To be subclassed by explicit measurement tasks
@@ -6,8 +8,11 @@ class Task:
     def __init__(self):
         self.taskname = "Superclass"
 
-    def record(self):
-        pass
+    def toDict(self):
+        return {'taskname':'superclass'}
+
+    def toJson(self):
+        return json.dumps(self.toDict())
 
     # this will be overridden by subclasses
     def run(self):
