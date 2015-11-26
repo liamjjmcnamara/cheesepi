222359bee791fee182a489ffcddb82f5081059b5
Liam McNamara <liamjjmcnamara@gmail.com>
2015-11-26 10:53:37 +0100

    task definitions as classes

---
+++ cheesepi/tasks/__init__.py
@@ -0,0 +1,22 @@
+
+from Task import Task
+from Ping import Ping
+from Httping import Httping
+from Traceroute import Traceroute
+
+import json
+
+def build_json(dao, json_str):
+    spec = json.loads(json_str)
+    return build_task(spec)
+
+def build_task(dao, spec):
+    if spec['taskname']=='ping':
+        return Ping(dao, spec)
+    if spec['taskname']=='httping':
+        return Httping(dao, spec)
+    if spec['taskname']=='traceroute':
+        return Traceroute(dao, spec)
+    else:
+        raise Exception('Task name not specified!')
+
