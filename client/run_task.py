fa16e29a1216f6d3d1f9fe1fbf124c4a39ceee00
Liam McNamara <liamjjmcnamara@gmail.com>
2016-01-06 13:17:08 +0100

    removed twisted from tun_task, not needed

---
+++ client/run_task.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals, absolute_import, print_function
+
+import argparse
+
+import cheesepilib as cp
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--task', type=str, default=None,
+                    help='Task spec')
+
+args = parser.parse_args()
+if args.task == None:
+	print("Error: missing --task argument")
+	exit(0)
+
+dao = cp.config.get_dao()
+task = cp.utils.build_json(dao, args.task)
+
+if task==None:
+	print("Task spec does not seem valid: "+args.task)
+	exit(1)
+
+task.run()
