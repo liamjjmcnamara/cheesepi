42669d2f7b4fd565fea97fd1b521f9e214f24006
Liam McNamara <liamjjmcnamara@gmail.com>
2016-01-13 17:17:18 +0100

    helper scripts

---
+++ client/start_influxdb.sh
@@ -0,0 +1,7 @@
+# Find containing directory
+CLIENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo $CLIENT_DIR
+
+# Start server
+$CLIENT_DIR/tools/influxdb/influxdb -config=$CLIENT_DIR/tools/infludb/config.toml
+
