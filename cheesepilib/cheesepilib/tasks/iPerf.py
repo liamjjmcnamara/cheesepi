a3aec55a48c0ed82c65ebfe2ca96eb1f125b0c0b
Liam McNamara <liamjjmcnamara@gmail.com>
2016-01-13 09:27:18 +0000

    updated to .spec format of param

--- cheesepilib/cheesepilib/tasks/iPerf.py
+++ cheesepilib/cheesepilib/tasks/iPerf.py
@@ -25,7 +25,7 @@ class iPerf(Task.Task):
 	# measure and record funtion
 	def measure(self):
 		self.spec['start_time'] = cp.utils.now()
-		op_output  = self.perform(self.landmark, self.port)
+		op_output  = self.perform(self.spec['landmark'], self.spec['port'])
 		self.spec['end_time']   = cp.utils.now()
 		logger.debug(op_output)
 		parsed_output = self.parse_output(op_output)
@@ -42,7 +42,7 @@ class iPerf(Task.Task):
 		return ret
 
 	#read the data from ping and reformat for database entry
-	def parse_output(self, data, landmark, start_time, end_time):
+	def parse_output(self, data):
 
 		lines = data.split("\n")
 		fields = lines[0].split(',')
