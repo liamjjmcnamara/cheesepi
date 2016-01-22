9adfc377f96f43a7725d227dddeb81245d61ef84
Gustaf Lindstedt <gustaflindstedt@gmail.com>
2016-01-22 13:56:33 +0100

    use tab for indentation

--- cheesepilib/cheesepilib/server/scheduling/PingScheduler.py
+++ cheesepilib/cheesepilib/server/scheduling/PingScheduler.py
@@ -7,52 +7,55 @@ from .Scheduler import Scheduler
 
 class PingScheduler(Scheduler):
 
-    def __init__(self, peer_id):
-        self.dao = MongoDAO('localhost', 27017)
-        self._peer_id = peer_id
+	def __init__(self, peer_id):
+		self.dao = MongoDAO('localhost', 27017)
+		self._peer_id = peer_id
 
-    def get_schedule(self, num=1):
-        schedule = []
+	def get_schedule(self, num=1):
+		schedule = []
 
-        stats = self.dao.get_all_stats(self._peer_id)
+		stats = self.dao.get_all_stats(self._peer_id)
 
-        from pprint import pformat
+		from pprint import pformat
 
-        #print(pformat(stats.toDict()))
+		#print(pformat(stats.toDict()))
 
-        priority_sorted_targets = []
+		priority_sorted_targets = []
 
-        for s in stats:
-            #print(pformat(s.toDict()))
-            delay_variance = s.get_delay().get_variance()
-            target = s.get_target()
-            #print(target)
-            #print(delay_variance)
-            heapq.heappush(priority_sorted_targets, (-delay_variance, target))
+		for s in stats:
+			#print(pformat(s.toDict()))
+			delay_variance = s.get_delay().get_variance()
+			target = s.get_target()
+			#print(target)
+			#print(delay_variance)
+			heapq.heappush(priority_sorted_targets, (-delay_variance, target))
 
-        #print(priority_sorted_targets)
+		#print(priority_sorted_targets)
 
-        for i in range(0, num):
-            target = heapq.heappop(priority_sorted_targets)
-            schedule.append(target[1])
+		for i in range(0, min(num,len(priority_sorted_targets))):
+			target = heapq.heappop(priority_sorted_targets)
+			schedule.append(target[1])
 
-        return schedule
+		return schedule
 
 
 if __name__ == "__main__":
-    import argparse
-    import cheesepilib.server.utils as utils
+	import argparse
+	import cheesepilib.server.utils as utils
 
-    utils.init_logging()
+	utils.init_logging()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--id', type=str, default=None)
-    parser.add_argument('--num', type=int, default=1)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--id', type=str, default=None)
+	parser.add_argument('--num', type=int, default=1)
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    ps = PingScheduler(args.id)
+	ps = PingScheduler(args.id)
 
-    schedule = ps.get_schedule(num=args.num)
-    for target in schedule:
-        print(target.get_uuid())
+	schedule = ps.get_schedule(num=args.num)
+	if len(schedule) == 0:
+		print("Nothing scheduled")
+	else:
+		for target in schedule:
+			print(target.get_uuid())
