5794e421d8b8624a50cb154003b2b52ca93cff68
Liam McNamara <liamjjmcnamara@gmail.com>
2016-02-02 18:17:38 +0100

    include influxdb as a 'script'

--- setup.py
+++ setup.py
@@ -30,22 +30,24 @@ a local config file (if one does not exist, and initialising logging options
 """
 
 from subprocess import call
-#from setuptools import setup, find_packages
-from distutils.core import setup
-from distutils.command.install import install
+from setuptools import setup, find_packages
+#from distutils.core import setup
+#from distutils.command.install import install
 
 def readme():
 	with open('README.rst') as f:
 		return f.read()
 
-class post_install(install):
-	def run(self):
-		install.run(self)
-		call(['python', 'scriptname.py'], cwd=self.install_lib + 'packagename')
+#class post_install(install):
+#	def run(self):
+#		install.run(self)
+#		print self.install_lib
+#		print self
+#		#call(['python', 'scriptname.py'], cwd=self.install_lib + 'packagename')
 
 setup(
 	name='cheesepi',
-	version='0.9.4',
+	version='0.9.10',
 	description='CheesePi Library',
 	long_description=readme(),
 	url='http://cheesepi.sics.se',
@@ -78,8 +80,9 @@ setup(
 		'uptime',
 		'cherrypy',
 	],
-	cmdclass={'install': post_install},
+	#cmdclass={'install': post_install},
 
+	scripts=['cheesepi/bin/tools/influxdb/influxdb'],
 	entry_points={
 		'console_scripts':[
 			'cheesepi = cheesepi.utils:console_script',
